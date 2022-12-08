601904311dc692ba9cae338a797ef2d51b00029e
tornikeia <!openSesame1>
2022-12-08 16:21:23 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useRef, useState } from "react";
 
 function App() {
+  const [isShown, setIsShown] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+  const inputRef = useRef(null);
+  const [updated, setUpdated] = useState("");
+
+  const handleClick = (e) => {
+    setIsShown((current) => !current);
+  };
+
+  const handleButton = (event) => {
+    setIsActive((current) => !current);
+  };
+
+  const handleMessage = () => {
+    setUpdated(inputRef.current.value);
+  };
+
+  let counter = 0;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="button_wrapper">
+        <button
+          className={isActive ? "display" : ""}
+          id="pre_order"
+          onClick={() => {
+            counter++;
+            console.log(counter);
+            handleClick();
+            handleButton();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          PRE-ORDER NOW
+        </button>
+        {isShown && (
+          <div className="answer">
+            <h2>
+              Currently the game is under development. If you want to get weekly
+              updates, subscribe to our newsletter
+            </h2>
+            <form className="email_wrapper">
+              <input ref={inputRef} type="text" id="message" name="message" />
+
+              {/* <h2>Updated: {updated}</h2> */}
+
+              <button className="update_message" onClick={handleMessage}>
+                Send
+              </button>
+            </form>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
